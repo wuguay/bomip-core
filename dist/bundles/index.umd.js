c741693a3d9882d07d987c82ec775f0f04ee812b
wuguay <wuguay@yahoo.com>
2017-11-07 14:18:23 -0800

    fix: rollup to compile package

--- dist/bundles/index.umd.js
+++ dist/bundles/index.umd.js
@@ -4,12 +4,6 @@
 	(factory((global.bp = global.bp || {}, global.bp.core = {}),global.core,global.common));
 }(this, (function (exports,core,common) { 'use strict';
 
-var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
 var isBlank = function (obj) { return obj === undefined || obj === null; };
 var BomipCoreModule = /** @class */ (function () {
     function BomipCoreModule() {
--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,19 @@
 export default {
-  format: 'umd',
+  output: {
+    format: 'umd'
+  },
   external: [
-       '@angular/core',
-       '@angular/common',
-       'meteor-rxjs',
-       'meteor/meteor',
-       'lodash',
-       'lodash/fp',
-       'reflect-metadata',
-       'rxjs',
-       'rxjs/observable/ArrayObservable',
-       'rxjs/observable/DeferObservable',
-       'rxjs/observable/EmptyObservable',
-       'rxjs/observable/ErrorObservable',
-       'rxjs/observable/ForkJoinObservable',
-       'rxjs/observable/FromEventObservable',
-       'rxjs/observable/FromEventPatternObservable',
-       'rxjs/observable/FromObservable',
-       'rxjs/observable/GenerateObservable',
-       'rxjs/observable/IfObservable',
-       'rxjs/observable/IntervalObservable',
-       'rxjs/observable/IteratorObservable',
-       'rxjs/observable/NeverObservable',
-       'rxjs/observable/PairsObservable',
-       'rxjs/observable/PromiseObservable',
-       'rxjs/observable/RangeObservable',
-       'rxjs/observable/ScalarObservable',
-       'rxjs/observable/SubscribeOnObservable',
-       'rxjs/observable/TimerObservable',
-       'rxjs/observable/UsingObservable',
-       'zone.js'
-   ],
+        '@angular/core',
+        '@angular/common',
+        'meteor-rxjs',
+        'meteor/meteor',
+        'lodash',
+        'lodash/fp',
+        'zone.js'
+    ],
   globals: {
-    '@angular/core':'core',
-    '@angular/common':'common',
-    'rxjs':'rxjs'
+    '@angular/core': 'core',
+    '@angular/common': 'common',
+    'rxjs': 'rxjs'
   }
 };
